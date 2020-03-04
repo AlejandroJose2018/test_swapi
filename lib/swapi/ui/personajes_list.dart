23ac97e85401981feb78a5130123bda16e448331
AlejandroJose <alejandrohachiko2018@gmail.com>
2020-03-03 19:04:14 -0500

    end test

--- lib/swapi/ui/personajes_list.dart
+++ lib/swapi/ui/personajes_list.dart
@@ -25,7 +25,7 @@ class MovieList extends StatelessWidget {
           } else if (snapshot.hasError) {
             return Text(snapshot.error.toString());
           }
-          return Center(child: CircularProgressIndicator());
+          return Center(child: CircularProgressIndicator(backgroundColor: AppTheme.mainColor1,));
         },
       ),
     );
@@ -33,55 +33,60 @@ class MovieList extends StatelessWidget {
 
   Widget buildList(AsyncSnapshot<ModelPersonaje> snapshot) {
     return GridView.builder(
-        itemCount: snapshot.data.results.length,
-        gridDelegate:
-            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: snapshot.data.results.length ,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-                  child: Container(
-                    margin: EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        AppTheme.mainColor2,
-                        AppTheme.mainColor2,
-                      ],
-                      begin: FractionalOffset(0.2, 0.0),
-                      end: FractionalOffset(1.0, 0.6),
-                      stops: [0.0, 0.6],
-                      tileMode: TileMode.mirror
-                  ),                    
-                     borderRadius: BorderRadius.all(Radius.circular(15)),
-                    shape: BoxShape.rectangle,
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(                        
-                          blurRadius: 15.0,
-                          offset: Offset(0.7, 7.0)
-                      )
-
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(snapshot.data.results[index].name,
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                  ),
-                  ),
-                  onTap: (){                    
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => 
-                        DetailsPersonaje(
-                          name: snapshot.data.results[index].name,
-                          height: snapshot.data.results[index].height,
-                          birth_year: snapshot.data.results[index].birth_year,
-                          genre: snapshot.data.results[index].gender,
-                          hair_color: snapshot.data.results[index].hair_color,
-                        )),
-                    );
-                  },
-                );          
+          return _listPersonajes(index, context, snapshot);
+         
         });
   }
+
+  _listPersonajes(int index, BuildContext context, AsyncSnapshot<ModelPersonaje> snapshot) {
+     return GestureDetector(
+              child: Container(
+                margin: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    AppTheme.mainColor2,
+                    AppTheme.mainColor2,
+                  ],
+                  begin: FractionalOffset(0.2, 0.0),
+                  end: FractionalOffset(1.0, 0.6),
+                  stops: [0.0, 0.6],
+                  tileMode: TileMode.mirror
+              ),                    
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                shape: BoxShape.rectangle,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(                        
+                      blurRadius: 15.0,
+                      offset: Offset(0.7, 7.0)
+                  )
+
+                ],
+              ),
+              child: Center(
+                child: Text(snapshot.data.results[index].name,
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                ),
+              ),
+              ),
+              onTap: (){                    
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => 
+                    DetailsPersonaje(
+                      name: snapshot.data.results[index].name,
+                      height: snapshot.data.results[index].height,
+                      birth_year: snapshot.data.results[index].birth_year,
+                      genre: snapshot.data.results[index].gender,
+                      hair_color: snapshot.data.results[index].hair_color, 
+                    )),
+                );
+              },
+            );          
+  }
+
 }
 
